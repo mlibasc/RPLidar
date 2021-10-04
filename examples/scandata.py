55538e9fbd6617d57f245b2197986d78b00211fe
mlibasc <li.mbasc@gmail.com>
2021-10-04 15:02:44 -0400

    added certainty factor to lidar and kinect

--- examples/scandata.py
+++ examples/scandata.py
@@ -62,24 +62,15 @@ def make_gamma():
 gamma = make_gamma()
 
 def findMaxDist(depth):
-    #dst = np.dstack((depth,depth,depth)).astype(np.uint8)
     depth[depth == 2047] = 0
     dst = cv2.GaussianBlur(depth, (11,11), 0)
     val = np.amax(dst)
 
-    #xMax = np.amaxnp.argmax(dst, axis=0)
-    #yMax = np.argmax(dst, axis=1)
-    
-    #print("max val", val)
-
-    print("Max X Coord: ", int(np.mean(np.where(dst == val)[0])))
-    print("var X Coord: ", int(np.sqrt(np.var(np.where(dst == val)[0]))))
+    #print("Max X Coord: ", int(np.mean(np.where(dst == val)[0])))
+    #print("var X Coord: ", int(np.sqrt(np.var(np.where(dst > (val-50))[0]))))
 
     #print("val at that place", depth[int(np.mean(np.where(dst == val)[0]))][np.where(dst == val)[1][0]])
 
-    # Record both x and y coorindates for max distance
-    # Draw circle over the coordinates later
-    # Return only xMax for now
     return int(np.mean(np.where(dst==val)[0])),int(np.sqrt(np.var(np.where(dst == val)[0])))
 
 def pwmDir(x):
@@ -88,10 +79,16 @@ def pwmDir(x):
     # when heading is the same as maxAngle, dc = 50%
     # 640 x 480 pixels
     
-    #left is greater than 50, right is less than 50
+    # kinect will only send out ~42 pwm to 58 pwm
+    # left is greater than 50, right is less than 50
+    # convert std of index to angle
+    #print('kinect std: ', x[1])
+    kinCert = x[1]/640 * 58.5
     dc = (x[0] / 640) * 17/2 + 50
-    print ('dc: ', dc)
-    return dc, x[1]
+    #print ('kinect dc: ', dc)
+    #print ('kin Cert: ', kinCert)
+    return dc, kinCert
+
 def update_line(num, iterator, line):
     scan = next(iterator)
     offsets = np.array([(np.radians(meas[1]), meas[2]) for meas in scan])
@@ -100,15 +97,31 @@ def update_line(num, iterator, line):
     line.set_array(intens)
     return line,
 
-def led(maxAngle):
+def led(maxAngle, rollAvg, maxDist, kinpwm, findMaxDist):
     # when heading is the same as maxAngle, dc = 50%
+    #cerfact = std/size of array * 360
+    lidstd = int(np.sqrt(np.var(np.where(rollAvg > (maxDist - 50)))))
+    lidCert = lidstd/len(rollAvg) * 360
+    liddc = (maxAngle / 360) * 100
+
+    kinstd = findMaxDist[1]
+    kinCert = kinpwm[1]
+    kindc = kinpwm[0]
+
+    print('kin std: ', kinstd)
+    print ('kin Cert: ', kinCert)
+    print ('kin dc: ', kindc)
     
-    dc = (maxAngle / 360) * 100
-    
-   # kinDc = glview.pwmDir[0]
-   # print('kinect dc: ', kinDc)
-    print ('lidar dc: ', dc)
-    pwm.ChangeDutyCycle(dc)
+    print('lid std: ', lidstd)
+    print ('lidar Cert: ', lidCert)
+    print ('lidar dc: ', liddc)
+
+    if(kinCert*10 > lidCert):
+        pwm.ChangeDutyCycle(kindc)
+        print('output kindc: ', kindc)
+    else:
+        pwm.ChangeDutyCycle(liddc)
+        print('output liddc: ', liddc)
     
 def obstacle(obDist, obAngle):
     if(obAngle <= 180):
@@ -132,6 +145,7 @@ def run():
             avgAngle = 0
             multA = 10 
             multB = 1
+            rollAvg = np.array([])
             
             # average the first 10 samples 
             for i in range(increment):
@@ -140,9 +154,12 @@ def run():
             avg /= increment
             avgAngle /= increment
 
+            rollAvg = np.append(rollAvg, avg)
+
             for i in range(increment, len(scan)):
                 avg = ((avg * multA) + (scan[i][2] * multB)) / (multA + multB)
                 avgAngle = ((avgAngle * multA) + (scan[i][1] * multB)) / (multA + multB)
+                rollAvg = np.append(rollAvg, avg)
                 if(avg > maxDist):
                     maxDist = avg
                     maxAngle = avgAngle
@@ -150,10 +167,10 @@ def run():
                     minAngle = avgAngle
 
             depth = np.rot90(get_depth()[0]) # get the depth readinngs from the camera
-            print("mypwmmm",pwmDir(findMaxDist(depth)))
+            #pwmDir(findMaxDist(depth))
             print('maxDist: ',  maxDist, " maxAngle: ", maxAngle)
             print('minDist: ', minDist, " minAngle: ", minAngle)
-            led(maxAngle)
+            led(maxAngle, rollAvg, maxDist, pwmDir(findMaxDist(depth)), findMaxDist(depth))
             #obstacle(minDist, minAngle)
             scan = next(iterator)
     lidar.stop()
